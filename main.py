42295712e58f9204120324be7c91dc603886e2bd
DevDeimosLsK <batdemas@outlook.com>
2024-09-12 18:29:04 -0500

    DEV_KRUIZ: Puntos 3, Punto 4, Punto 5 y Punto 6 Mineria de datos Primer momento
    Puntos 3, Punto 4, Punto 5 y Punto 6 Mineria de datos Primer momento

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 """
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors  # Importar el módulo de colores
 import seaborn as sns
+import numpy as np
 from bokeh.plotting import figure, show, output_file
+#import sklearn
 
 
 # Cargar el archivo CSV en un DataFrame
@@ -21,14 +24,6 @@ df_modificable = df_original.copy()
     Referencia del repositorio: https://github.com/VictorGuevaraP/Mineria-de-datos/blob/master/DAT%20SALUD%20MENTAL%20-%20ATENDIDOS.csv
 """
 
-
-"""
-    2. Eliminar variables irrelevantes y redundantes
-
-    Eliminar variables irrelevantes para la minería
-    -   Eliminamos el Distrito por que ya es un dato muy especifico de una localidad exacta dentro de la información, y esta se puede generalizar por cada una de las provincias de la misma
-"""
-df_modificable = df_modificable.drop(columns=['Distrito'])
 """
     Eliminar variables redundantes
     -   En este paso no se realiza eliminación de datos redundantes, ya que esta trae información limpia y sin redundar en la información puesto que todas son informaciones de diferentes procesos que se realizarón dentro de la inscripción de los pacientes como de su proceso en la entidad IPS en su debida ubicación
@@ -63,8 +58,6 @@ json_Diagnosticos_valor_unico = {Diagnostico: idx for idx, Diagnostico in enumer
     Mapa/Json de Etapas: json_Etapas_valor_unico    
     Mapa/Json de Diagnosticos: json_Diagnosticos_valor_unico
 """
-
-print(json_Etapas_valor_unico)
 # Modificar los valores en 'Departamento' usando el diccionario 'departamento_valor_unico'
 df_modificable['Departamento'] = df_modificable['Departamento'].map(json_departamento_valor_unico)
 df_modificable['Provincia'] = df_modificable['Provincia'].map(json_provincias_valor_unico)
@@ -78,12 +71,43 @@ etapas_valor_texto = {v: k for k, v in json_Etapas_valor_unico.items()}
 # Reemplazar los valores numéricos en la columna 'Etapa' con sus equivalentes en texto
 df_modificable['Etapa_Texto'] = df_modificable['Etapa'].map(etapas_valor_texto)
 
+
+# Resumen estadístico de la columna 'Atendidos'
+#resumen_estadistico = df_modificable['Atendidos'].describe()
+#print(resumen_estadistico)
+
+# Calcular estadísticas de la columna 'Atendidos'
+#media = df_modificable['Atendidos'].mean()  # Media
+#mediana = df_modificable['Atendidos'].median()  # Mediana
+#moda = df_modificable['Atendidos'].mode()[0]  # Moda
+#desviacion_estandar = df_modificable['Atendidos'].std()  # Desviación estándar
+#varianza = df_modificable['Atendidos'].var()  # Varianza
+#maximo = df_modificable['Atendidos'].max()  # Máximo valor
+#minimo = df_modificable['Atendidos'].min()  # Mínimo valor
+#rango = maximo - minimo  # Rango
+
+# Mostrar resultados
+#print("Media:", media)
+#print("Mediana:", mediana)
+#print("Moda:", moda)
+#print("Desviación Estándar:", desviacion_estandar)
+#print("Varianza:", varianza)
+#print("Máximo:", maximo)
+#print("Mínimo:", minimo)
+#print("Rango:", rango)
+
+"""
+3. Descripción estadística de los datos
+
+Se realiza el analisis de la información utilizando histograma en este caso para evaluar la variable de Etapa
+Se realiza el analisis de atendidos por departamentos
+"""
 # Crear un histograma donde X sea la variable 'Etapa_Texto'
 plt.figure(figsize=(10, 6))
 sns.histplot(df_modificable['Etapa_Texto'], bins=len(json_Etapas_valor_unico), color='skyblue', edgecolor='black')
 
 # Ajustar el título y etiquetas
-plt.title('Histograma por Etapas de Edad')
+plt.title('Histograma de frecuencia por Etapas de Edad')
 plt.xlabel('Etapas de Edad')
 plt.ylabel('Frecuencia')
 
@@ -93,3 +117,102 @@ plt.xticks(rotation=45)
 # Mostrar el gráfico
 plt.tight_layout()
 plt.show()
+
+# Agrupar por Provincia y sumar los atendidos
+grouped_data = df_modificable.groupby('Departamento')['Atendidos'].sum()
+
+# Crear gráfico de torta
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Colores para el gráfico
+colors = plt.cm.Paired.colors
+
+# Graficar la torta y mostrar los números de los Departamentos
+departamentos = df_modificable['Departamento'].unique()  # Números de departamento para las etiquetas
+wedges, texts = ax.pie(grouped_data, labels=departamentos, startangle=90, colors=colors)
+
+# Crear la tabla de resumen con el porcentaje y el color
+# Obtener los colores en formato hexadecimal (usamos get_facecolor() y convertimos correctamente)
+colors_hex = [mcolors.rgb2hex(wedge.get_facecolor()[:3]) for wedge in wedges]  # Tomamos los primeros 3 valores (RGB)
+
+# Crear la información de la tabla (excepto los colores, que se pintarán)
+table_data = [[f'{Departamento}', f'{atendido}'] for Departamento, atendido in zip(grouped_data.index, grouped_data)]
+
+# Agregar tabla a la gráfica
+tabla = ax.table(cellText=table_data, colLabels=["Departamento", "Atendidos"], loc="right", colColours=['#f5f5f5']*2, cellLoc='center')
+
+# Pintar las celdas de la columna de colores
+for i, color in enumerate(colors_hex):
+    tabla.add_cell(i+1, 2, width=0.1, height=0.05, facecolor=color)  # Agregar celda pintada con el color respectivo
+
+# Agregar la columna de título "Color"
+tabla.add_cell(0, 2, width=0.1, height=0.05, text="Color", loc='center', facecolor='#f5f5f5')
+
+# Ajustar la posición del gráfico para que la tabla no se superponga
+plt.subplots_adjust(left=0.1, right=0.7)
+
+plt.title('Cantidad de Pacientes Atendidos por Departamento')
+plt.show()
+
+"""
+    2. Eliminar variables irrelevantes y redundantes
+
+    Eliminar variables irrelevantes para la minería
+    -   Eliminamos el Distrito por que ya es un dato muy especifico de una localidad exacta dentro de la información, y esta se puede generalizar por cada una de las provincias de la misma
+    -   Eliminamos el Anio debido a que este no representa variación segun la base de datos, por lo que nos generara en la correlación de información un valor NaN debido a la variación del mismo
+    
+"""
+df_modificable = df_modificable.drop(columns=['Distrito'])
+df_modificable = df_modificable.drop(columns=['Anio'])
+df_modificable = df_modificable.drop(columns=['ubigeo'])
+df_modificable = df_modificable.drop(columns=['Provincia'])
+df_modificable = df_modificable.drop(columns=['Sexo'])
+
+
+"""
+ 4. Limpieza de la información
+    Se hace la busqueda de datos que se encuentren nulos o esten vacias dentro de todas las columnas y se trae el index de la respectiva columna y el campo que hace falta
+    En este caso el propio proyecto nos indica que no se encontraron informaciones con valores nan , null o cadenas vacias
+"""
+# Buscar valores nulos
+nulos = df_modificable.isna()
+
+# Usar apply para aplicar una función map a cada columna y detectar cadenas vacías
+vacios = df_modificable.apply(lambda col: col.map(lambda x: isinstance(x, str) and x.strip() == ''))
+
+# Crear un nuevo DataFrame combinando nulos y vacíos
+nulos_o_vacios = nulos | vacios
+
+# Filtrar filas donde haya al menos un valor nulo o vacío
+df_nulos_vacios = df_modificable[nulos_o_vacios.any(axis=1)]
+
+# Mostrar el DataFrame con los valores nulos o vacíos
+#print(df_nulos_vacios)
+
+"""
+    6. Analisis de Correlación entre las variables
+
+    Se realiza el analisis de las variables referentes a las variables analisadas en la parte de estadistica descriptiva
+"""
+
+#Eliminamos las variables que utilizamos para los graficos
+df_modificable = df_modificable.drop(columns=['Etapa_Texto'])
+
+# Calcular la matriz de correlación
+correlacion = df_modificable.corr()
+
+# Mostrar la matriz de correlación
+print(correlacion)
+"""
+
+    Metodo del codo para definir la cantidad de cluster en mi algoritmo de predicción
+    Atraves del algoritmo de Kmeans y atraves de su propiedad de inercia .inertia_ ( calculo de error )
+    se grafica de manera lineal y de puntos
+
+    El numero de cluster es donde la linea empiece a tomar una linea recta ( se suaviza una curva )
+
+    Los centroides son la cantidad de variables a evaluar
+
+    La reduccion de la dimencionalidad a los datos esto con
+    sklearn.decomposition importado de PCA
+"""
